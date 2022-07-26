4d433da3e55aed55d53c5741cbbf6dd7ffb944eb
Harllem Nascimento <harllemnascimento@gmail.com>
2022-07-26 10:53:34 -0300

    Implementado Cadastro, Listagem e Edição da Seção 08 - Departamentos

--- src/main/java/com/tiduswr/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/tiduswr/curso/boot/web/controller/DepartamentoController.java
@@ -1,21 +1,57 @@
 package com.tiduswr.curso.boot.web.controller;
 
+import com.tiduswr.curso.boot.domain.Departamento;
+import com.tiduswr.curso.boot.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 
+    @Autowired
+    private DepartamentoService service;
+
     @GetMapping("/cadastrar")
-    public String cadastrar(){
+    public String cadastrar(Departamento d){
         return "/departamento/cadastro";
     }
 
     @GetMapping("/listar")
-    public String listar(){
+    public String listar(ModelMap model){
+        List<Departamento> l = service.buscarTodos();
+        if(l != null){
+            l = l.stream()
+                .sorted((o1, o2) -> o1.getId().compareTo(o2.getId()))
+                .collect(Collectors.toList());
+        }
+        model.addAttribute("departamentos", l);
         return "/departamento/lista";
     }
 
+    @PostMapping("/salvar")
+    public String salvar(Departamento d){
+        service.salvar(d);
+        return "redirect:/departamentos/cadastrar";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String preEditar(@PathVariable Long id, ModelMap model){
+        model.addAttribute("departamento", service.buscarPorId(id));
+        return "/departamento/cadastro";
+    }
+
+    @PostMapping("/editar")
+    public String editar(Departamento d){
+        service.editar(d);
+        return "redirect:/departamentos/listar";
+    }
 }
